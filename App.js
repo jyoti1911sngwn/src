499bbd0e54444d1b18f722c46d4ab6e8bdfac9a1
jyoti sangwan <jyotisangwan@jyotis-Air.lan>
2025-12-16 21:21:16 +0530

    api dat

--- App.js
+++ App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
-import { useState } from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+// import { useState } from 'react';
 
-function App() {
-  const currentItem = 3
-  const [page, setpage] = useState(1)
-  const arr = ["1abc", "2abc", "3abc","4abc", "5abc", "6abc","7abc", "8abc", "9abc","10abc", "11abc", "12abc"]
-  const newarr = arr.slice( ((page * currentItem)-3), page * currentItem)
-  console.log(page )
+// function App() {
+//   const currentItem = 3
+//   const [page, setpage] = useState(1)
+//   const arr = ["1abc", "2abc", "3abc","4abc", "5abc", "6abc","7abc", "8abc", "9abc","10abc", "11abc", "12abc"]
+//   const newarr = arr.slice( ((page * currentItem)-3), page * currentItem)
+//   console.log(page )
 
+//   return (
+//     <div className="App">
+//      {newarr.map((val,idx)=>(
+//       <div>{val}</div>
+//      ))}
+//      <button onClick={()=>setpage(page-1)}>Prev</button>
+//      <button onClick={()=>setpage(page+1)}>Next</button>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React, { useEffect, useState } from 'react'
+
+const App = () => {
+  const[result, setresult] = useState([])
+  const [page,setPage] =useState(1)
+  const currentPage = 10 ;
+  const func = async()=>{
+    const data = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const res = await data.json()
+    setresult(res)
+  }
+  const currentRes = result.slice((page*currentPage- currentPage), page*currentPage)
+  useEffect(()=>{
+    func()
+  },[])
   return (
-    <div className="App">
-     {newarr.map((val,idx)=>(
-      <div>{val}</div>
-     ))}
-     <button onClick={()=>setpage(page-1)}>Prev</button>
-     <button onClick={()=>setpage(page+1)}>Next</button>
+    <div>
+      {currentRes.map((val,idx)=>(
+        <div>{val.title}</div>
+      ))}
+      <button onClick={()=> setPage(page-1)}>Prev</button>
+      <button onClick={()=> setPage(page+1)}>next</button>
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+
